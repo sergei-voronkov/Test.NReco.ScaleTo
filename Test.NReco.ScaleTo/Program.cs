40db157765a16a217f39388e2e422beec2a703e9
Sergei Voronkov <sergei.voronkov@stosolutions.com>
2022-07-13 15:56:39 +0200

    Add project files.

---
+++ Test.NReco.ScaleTo/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Test.NReco.ScaleTo
+{
+    using global::NReco.PdfRenderer;
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            new PdfToImageConverter
+            {
+                ScaleTo = 2200
+            }
+            .GenerateImage(
+                "sample.pdf",
+                1,
+                ImageFormat.Png,
+                "sample.pdf.png");
+        }
+    }
+}
